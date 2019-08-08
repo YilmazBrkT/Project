d73aa937034df465ff7468dae675f0bcb54bbef9
Yilmaz <yilmazbarkut@outlook.fr>
2019-08-08 14:39:13 +0200

    Probleme Tuto

---
+++ config.js
@@ -0,0 +1,2 @@
+exports.TOKEN = "NjA4OTgzNTI5NzE1NTMxNzc2.XUwKGw.ztISCoyf_9vqF387VHxpmdw7uwA";
+exports.PREFIX = "s";
--- main.js
+++ main.js
@@ -1,16 +1,14 @@
 const { Client } = require("discord.js");
+const { TOKEN, PREFIX } = require("./config");
 const client = new Client({ disableEveryone: true });
 
 client.on("ready", () => {
-  console.log(`${client.user.tag} est connecté !`);
+  console.log("Sven est connecté !");
 });
 
 client.on("message", msg => {
-  if (msg.content === "Sven ?") msg.channel.send("AHAHAHAH PUTE");
-  if (msg.content === "everyone")
-    msg.channel.send("@everyone, salut à tous !", { disableEveryone: false });
-  if (msg.content === "noteveryone")
-    msg.channel.send("@everyone (noteveryone), salut à tous !");
+  if (msg.content.startsWith(`${PREFIX}Sven ?`))
+    msg.channel.send("WOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOF");
 });
 
-client.login("NjA4OTgzNTI5NzE1NTMxNzc2.XUwKGw.ztISCoyf_9vqF387VHxpmdw7uwA");
+client.login(TOKEN);
